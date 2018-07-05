75740c6619a9860114116ba77121130bdc47cb59
Mohsenkhodadadzadeh <black.roze2020@gmail.com>
2018-07-05 12:19:38 +0430

    add replay subject and edit example func

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,7 +5,7 @@ import RxSwift
 
 
 public func example(of description: String, action: () -> Void) {
-    print("\n Example of : " , description , "-----")
+    print("\n----- Example of : " , description , "-----")
     action()
 }
 
@@ -44,5 +44,48 @@ example(of : "PublishSubject") {
 
 enum myError: Error {
     case anError_Code
+}
+
+example(of: "BehaviorSubject") {
+    let subject = BehaviorSubject(value: "Inital Value")
+    
+    let disposeBag = DisposeBag()
+    
+    subject.on(.next("second Initial"))
+    subject.subscribe { event in
+        print("behavior 1 : " , event.element ?? event)
+    }.disposed(by: disposeBag)
+    
+    subject.on(.error(myError.anError_Code))
+    subject.subscribe { event in
+        print("behavior 2 :" , event.element ?? event)
+    }
+}
+
+example(of: "Replay Subject") {
+    
+    let subject = ReplaySubject<String>.create(bufferSize: 2)
+    
+    let disposeBag = DisposeBag()
+    
+    subject.onNext("First Element")
+    subject.onNext("Second Element")
+    subject.onNext("thierd Element")
+    
+    subject.subscribe {
+        print("First Subscribe : " , $0.element ?? $0)
+        }.disposed(by: disposeBag)
+    
+    subject.subscribe {
+        print("Second Subescribe : " , $0.element ?? $0)
+        }.disposed(by: disposeBag)
+    subject.onNext("fourth element")
+    subject.onError(myError.anError_Code)
+    subject.subscribe {
+        print("Thierd Replay : " , $0.element ?? $0)
+        }.disposed(by: disposeBag)
+    
+    
+    
 }
 
